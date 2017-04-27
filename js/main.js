ace9ce9ee0ed4d1955ae6d257dac36b25d66a1b3
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-27 12:49:20 -0400

    add jQuery ready function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-// $(document).ready(function() {
-//   // CODE GOES HERE
-// });
+$(document).ready(function() {
+  // CODE GOES HERE
+  // only once document is ready!
+  var title = document.getElementById('title');
+  title.style.color = "#FF0";
 
-var title = document.getElementById('title');
-title.style.color = "yellow";
+});
